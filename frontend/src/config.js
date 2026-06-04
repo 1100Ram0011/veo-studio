f9799a2cfcfe06b3beea746cf6cb933ecfa56395
1100Ram0011 <rambachanyadav558@gmail.com>
2026-06-04 23:14:07 +0530

    second commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-// Development mein localhost, Production mein Railway URL
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+// Purani line ko hata kar seedha ye likh dein:
+const API_URL = 'https://veo-studio-jk43.onrender.com'
 
 export default API_URL
