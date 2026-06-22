0dc0af2cabb31533d6956606f5bb5f5164cca898
Rambachan Yadav <111169032+1100Ram0011@users.noreply.github.com>
2026-06-22 12:37:51 +0530

    Update image.js

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const imageController = require('../controllers/imageController')
+const { authMiddleware, requireCredits } = require('../middleware/authMiddleware')
 
 // Yahan imageController.generateImage likhna compulsory hai
-router.post('/generate', imageController.generateImage)
+router.post('/generate', authMiddleware, requireCredits(1), imageController.generateImage)
 
 module.exports = router
