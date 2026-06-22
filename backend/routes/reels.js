14d9ff7889ef8cded80680dcb6c07839c4b2b1f2
Rambachan Yadav <111169032+1100Ram0011@users.noreply.github.com>
2026-06-22 12:38:34 +0530

    Update reels.js

--- backend/routes/reels.js
+++ backend/routes/reels.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const reelsController = require('../controllers/reelsController')
+const { authMiddleware, requireCredits } = require('../middleware/authMiddleware')
 
-router.post('/generate', reelsController.generateReels)
+router.post('/generate', authMiddleware, requireCredits(1), reelsController.generateReels)
 router.post('/result', reelsController.getReelsResult)
 
 module.exports = router
